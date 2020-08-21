dbe82d7b26e135b3d14f297bf7d582a23c31fbfd
wyx <YongXiang_Wang@vtech.com>
2020-08-21 14:54:34 +0800

    update min_depth

--- record.java
+++ record.java
@@ -328,6 +328,19 @@ public class record{
         }
     }
 
+    public int minDepth(TreeNode root) {
+        if(root==null) return 0;
+        if(root.left ==null && root.right==null) return 1;
+        int min_depth = Integer.MAX_VALUE;
+        if(root.left != null){
+            min_depth = Math.min(min_depth,minDepth(min_depth.left));
+        }
+        if(root.right != null){
+            min_depth = Math.min(min_depth,minDepth(min_depth.right));
+        }
+        return min_depth+1;
+    }
+
 }
 class ListNode {
     int val;
