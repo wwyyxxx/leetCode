4e46b1ad2bccf47b1d5339d16926cc83d4eab005
Tungbo <wangyongxiang@vspn.cn>
2021-07-05 18:25:33 +0800

    打印从1到最大的n位数

---
+++ java/printNumbers.java
@@ -0,0 +1,16 @@
+/*
+ * @Author: Tungbo
+ * @Date: 2021-07-05 18:25:00
+ * @LastEditTime: 2021-07-05 18:25:29
+ * @LastEditors: Tungbo
+ * @Description: leecode: 打印从1到最大的n位数
+ */
+public class printNumbers{
+    public int[] printNumbers(int n) {
+        int[] res = new int[(int)Math.pow(10,n)-1];
+         for(int i =0;i<res.length;i++) {
+             res[i] = i+1;
+         }
+        return res;
+    }
+}
