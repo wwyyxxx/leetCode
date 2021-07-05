cd4aaa0e445e2fd5ad4a483f5ca557212dd35475
Tungbo <wangyongxiang@vspn.cn>
2021-07-05 18:53:24 +0800

    删除链表的节点

---
+++ java/deleteNode.java
@@ -0,0 +1,21 @@
+/*
+ * @Author: Tungbo
+ * @Date: 2021-07-05 18:52:58
+ * @LastEditTime: 2021-07-05 18:53:16
+ * @LastEditors: Tungbo
+ * @Description: leecode: 删除链表的节点
+ */
+class Solution {
+    public ListNode deleteNode(ListNode head, int val) {
+        ListNode cur = head;
+        if(head.val == val) return head.next;
+        while(head.next != null) {
+            if(head.next.val == val) {
+                head.next = head.next.next;
+            } else {
+                head = head.next;
+            }
+        }
+        return cur;
+    }
+}
