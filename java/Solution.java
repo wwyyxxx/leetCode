0e57df2e41040f5b93c958dc2196ee26fc430ef9
Tungbo <wangyongxiang@vspn.cn>
2021-07-12 16:01:31 +0800

    顺时针打印矩阵

--- java/Solution.java
+++ java/Solution.java
@@ -1,7 +1,7 @@
 /*
  * @Author: Tungbo
  * @Date: 2021-07-06 14:44:26
- * @LastEditTime: 2021-07-12 14:34:01
+ * @LastEditTime: 2021-07-12 16:01:28
  * @LastEditors: Tungbo
  * @Description: leecode: leetcode记录
  */
@@ -248,6 +248,43 @@ public class Solution {
                 left++;
             }
         }
+    }
+
+    // 顺时针打印矩阵
+    public int[] spiralOrder(int[][] matrix) {
+        if(matrix.length == 0 || matrix[0].length == 0) return new int[0];
+        int left = 0, r = matrix[0].length -1, top = 0, row = matrix.length - 1, x=0;
+        int[] result = new int[(row+1)*(r+1)];
+        while(true) {
+            for(int i = left;i<=r;i++) result[x++] = matrix[top][i]; //左到右
+            if(++top > row) break;
+
+            for(int i = top;i<=row;i++) result[x++] = matrix[i][r]; //上到下
+            if(left > --r) break;
+
+            for(int i = r;i>=left;i--) result[x++] = matrix[row][i]; //右到左
+            if(top > --row) break;
+            
+            for(int i = row;i>=top;i--) result[x++] = matrix[i][left]; //下到上
+            if(++left > row) break;
+        }
+        return result;
+    }
+    public int[] spiralOrder(int[][] matrix) {
+        if(matrix.length == 0) return new int[0];
+        int l = 0, r = matrix[0].length - 1, t = 0, b = matrix.length - 1, x = 0;
+        int[] res = new int[(r + 1) * (b + 1)];
+        while(true) {
+            for(int i = l; i <= r; i++) res[x++] = matrix[t][i]; // left to right.
+            if(++t > b) break;
+            for(int i = t; i <= b; i++) res[x++] = matrix[i][r]; // top to bottom.
+            if(l > --r) break;
+            for(int i = r; i >= l; i--) res[x++] = matrix[b][i]; // right to left.
+            if(t > --b) break;
+            for(int i = b; i >= t; i--) res[x++] = matrix[i][l]; // bottom to top.
+            if(++l > r) break;
+        }
+        return res;
     }
 }
 
