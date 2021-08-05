d2ba9f4a30cf485e6baaa60195ebf5ff45602ad0
Tungbo <wangyongxiang@vspn.cn>
2021-08-05 15:09:22 +0800

    平衡二叉树

--- java/Solution.java
+++ java/Solution.java
@@ -559,6 +559,19 @@ public class Solution {
         return res.toArray(new int[0][]);
     }
 
+    // 平衡二叉树
+    public boolean isBalanced(TreeNode root) {
+        return recur(root) != -1;
+    }
+
+    private int recur(TreeNode root) {
+        if(root == null) return 0;
+        int left = recur(root.left);
+        if(left == -1) return -1;
+        int right = recur(root.right);
+        if(right == -1) return -1;
+        return Math.abs(left - right) < 2 ? Math.max(left,right) + 1 : -1;
+    }
 }
 
 class TreeNode {
