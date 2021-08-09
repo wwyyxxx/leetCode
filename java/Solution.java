9087b4536d4165c3008ec82ca9a878c6eca61654
Tungbo <wangyongxiang@vspn.cn>
2021-08-09 11:07:16 +0800

    0～n-1中缺失的数字

--- java/Solution.java
+++ java/Solution.java
@@ -635,6 +635,17 @@ public class Solution {
         int left = j;
         return right - left - 1;
     }
+
+    // 0～n-1中缺失的数字
+    public int missingNumber(int[] nums) {
+        int i = 0, j = nums.length -1;
+        while(i <= j) {
+            int m = (i + j) / 2;
+            if(nums[m] > m) j = m - 1;
+            else i = m + 1;
+        }
+        return i;
+    }
 }
 
 class TreeNode {
