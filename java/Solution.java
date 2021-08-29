16464547df738d5258d466a1f8c08866378474ab
Tungbo <1023589809@qq.com>
2021-08-29 23:00:47 +0800

    数字序列中某一位的数字

--- java/Solution.java
+++ java/Solution.java
@@ -800,4 +800,19 @@ public class Solution {
         dfsTreeToDoublyList(cur.right);
     }
 
+
+    // 数字序列中某一位的数字
+    public int findNthDigit(int n) {
+        int digit = 1;
+        long start = 1;
+        long count = 9;
+        while (n > count) { // 1.
+            n -= count;
+            digit += 1;
+            start *= 10;
+            count = digit * start * 9;
+        }
+        long num = start + (n - 1) / digit; // 2.
+        return Long.toString(num).charAt((n - 1) % digit) - '0'; // 3.
+    }
 }
