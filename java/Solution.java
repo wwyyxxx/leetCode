8c90b9a29e3352b65ce93abf4e07f875ac1eb81d
Tungbo <wangyongxiang@vspn.cn>
2021-09-15 15:54:47 +0800

    n个骰子的点数

--- java/Solution.java
+++ java/Solution.java
@@ -946,4 +946,20 @@ public class Solution {
         }
         return sign * res;
     }
+
+    // n个骰子的点数
+    public double[] dicesProbability(int n) {
+        double[] dp = new double[6];
+        Arrays.fill(dp, 1.0 / 6.0);
+        for (int i = 2; i <= n; i++) {
+            double[] tmp = new double[5 * i + 1];
+            for (int j = 0; j < dp.length; j++) {
+                for (int k = 0; k < 6; k++) {
+                    tmp[j + k] += dp[j] / 6.0;
+                }
+            }
+            dp = tmp;
+        }
+        return dp;
+    }
 }
