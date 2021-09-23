7741b2f6f20953f14a77876265ca736302cf9953
Tungbo <wangyongxiang@vspn.cn>
2021-09-23 16:01:44 +0800

    1～n 整数中 1 出现的次数

--- java/Solution.java
+++ java/Solution.java
@@ -1003,4 +1003,21 @@ public class Solution {
         }
         return res;
     }
+
+    // 1～n 整数中 1 出现的次数
+    public int countDigitOne(int n) {
+        int digit = 1, res = 0;
+        int high = n/10, cur = n % 10, low = 0;
+        while(high != 0 || cur != 0) {
+            if(cur == 0) res += digit * high;
+            else if(cur == 1) res += high * digit + low + 1;
+            else res += (high + 1) * digit;
+            low += cur * digit;
+            cur = high % 10;
+            high /= 10;
+            digit *= 10;
+        }
+        return res;
+    }
+
 }
