03d0963e27efad1222c3f5bf5d4138a684a529d9
Tungbo <1023589809@qq.com>
2021-12-12 18:37:23 +0800

    允许重复选择元素的组合

--- java/Solution.java
+++ java/Solution.java
@@ -1142,4 +1142,27 @@ public class Solution {
         return false;
     }
 
+    //允许重复选择元素的组合
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+        dfs(0,candidates,res,temp,target);
+        return res;
+    }
+
+
+    private void dfs(int i, int[] candidates, List<List<Integer>> res2, List<Integer> temp2, int target) {
+        if(target < 0 || i <=candidates.length) {
+            return;
+        }
+        if(target == 0) {
+            res2.add(new ArrayList<>(temp2));
+            return;
+        }
+        temp2.add(candidates[i]);
+        dfs(i, candidates, res2, temp2, target-candidates[i]);
+        temp2.remove(temp2.size()-1);
+        dfs(i+1,candidates,res2,temp2,target);
+    }
+
 }
