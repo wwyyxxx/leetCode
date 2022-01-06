5adcf86b9d6d25c1e0807450bebc640384d96f85
Tungbo <wangyongxiang@vspn.cn>
2022-01-06 17:50:34 +0800

    房屋偷盗

--- java/Solution.java
+++ java/Solution.java
@@ -22,7 +22,7 @@ import bean.*;
 public class Solution {
 
     public static void main(String[] args) {
-        System.out.println(new Solution().numSubarrayProductLessThanK(new int[]{10,9,10,4,3,8,3,3,6,2,10,10,9,3},19));
+        System.out.println(new Solution().rob(new int[]{2,1,1,2}));
     }
 
 
@@ -1672,5 +1672,17 @@ public class Solution {
             }
         }
         return ans;
+    }
+
+    // 房屋偷盗   [2,1,1,2]
+    public int rob(int[] nums) {
+        if(nums.length < 2) return nums[0];
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(dp[0], nums[1]);
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(nums[i]+dp[i-2], dp[i-1]);
+        }
+        return dp.length == 2 ? Math.max(dp[0], dp[1]) : dp[nums.length - 1];
     }
 }
