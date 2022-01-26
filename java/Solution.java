1b301b226254cbca45f36eeb8d4433574b3b4252
Tungbo <1023589809@qq.com>
2022-01-26 23:28:37 +0800

    最多删除一个字符得到回文

--- java/Solution.java
+++ java/Solution.java
@@ -1803,4 +1803,25 @@ public class Solution {
         }
         return true;
     }
+
+    // 最多删除一个字符得到回文
+    public boolean validPalindrome(String s) {
+        int l = 0, r = s.length() -1;
+        while(l <r) {
+            if(s.charAt(l) != s.charAt(r)) {
+                return dfsPalindeome(l+1, r, s) ||dfsPalindeome(l, r-1, s); 
+            }
+        }
+        return true;
+    }
+
+    private boolean dfsPalindeome(int l,int r,String s) {
+        if(l == r) return true;
+        while(l < r) {
+            if(s.charAt(l)!= s.charAt(r)) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
 }
