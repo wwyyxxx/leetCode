dbb9de00d2a13b5adef50fea7fcc4b951cb9fea0
Tungbo <wangyongxiang@vspn.cn>
2022-02-08 18:28:14 +0800

    最长公共子序列

--- java/Solution.java
+++ java/Solution.java
@@ -22,7 +22,7 @@ import bean.*;
 public class Solution {
 
     public static void main(String[] args) {
-        System.out.println(new Solution().isPalindrome( "A man, a plan, a canal: Panama"));
+        System.out.println(new Solution().isAnagram("a","a"));
     }
 
 
@@ -1890,5 +1890,33 @@ public class Solution {
             head = head.next;
         }
         return true;
+    }
+
+    // 有效的变位词
+    public boolean isAnagram(String s, String t) {
+        if(s.equals(t) || s.length() != t.length()) return false;
+        char[] sch = s.toCharArray();
+        char[] tch = t.toCharArray();
+        Arrays.sort(sch);
+        Arrays.sort(tch);
+        return Arrays.equals(sch, tch);
+    }
+
+    // 最长公共子序列
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length(), n = text2.length();
+        int[][] dp = new int[m+1][n+1];
+        for (int i = 1; i <= m; i++) {
+            char c1 = text1.charAt(i);
+            for(int j = 1; j <= n; j++) {
+                char c2 = text2.charAt(j);
+                if(c1 == c2) {
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[m+1][n+1];
     }
 }
