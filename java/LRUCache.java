b136b062d2f38d818d8a191c97b869c9759881d3
Tungbo <wangyongxiang@vspn.cn>
2022-04-14 17:31:55 +0800

    LRU 缓存

---
+++ java/LRUCache.java
@@ -0,0 +1,30 @@
+/*
+ * @Author: Tungbo
+ * @Date: 2022-04-14 17:18:24
+ * @LastEditTime: 2022-04-14 17:31:11
+ * @LastEditors: Tungbo
+ * @Description: leecode: 
+ */
+import java.util.LinkedHashMap;
+import java.util.Map;
+//LRU 缓存
+public class LRUCache {
+    LinkedHashMap<Integer, Integer> map;
+    int maxSize;
+    public LRUCache(int capacity) {
+        map = new LinkedHashMap<>(capacity,0.75f,true);
+        maxSize = capacity;
+    }
+    
+    public int get(int key) {
+        return map.getOrDefault(key,-1);
+    }
+    
+    public void put(int key, int value) {
+        map.put(key,value);
+        if(map.size() > maxSize) {
+            Map.Entry<Integer, Integer> e = map.entrySet().iterator().next();
+            map.remove(e.getKey());
+        }
+    }
+}
