d0746eb9ded4f3707eba70f740dbf5543e89d709
Tungbo <wangyongxiang@vspn.cn>
2022-04-29 16:22:07 +0800

    最长有效括号

--- java/Solution.java
+++ java/Solution.java
@@ -2801,4 +2801,35 @@ public class Solution {
         }
         return ans;
     }
+
+    // 最长有效括号
+    public int longestValidParentheses(String s) {
+        int left = 0, right = 0, ans = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '(') {
+                left++;
+            } else {
+                right++;
+            }
+            if (left == right) ans = Math.max(ans,right *2);
+            else if (left < right) left = right = 0;
+        }
+        for (int i = s.length() -1; i >= 0; i--) {
+            if(s.charAt(i) == '(') {
+                left++;
+            } else {
+                right++;
+            }
+            if (left == right) ans = Math.max(ans, left *2);
+            else if (left > right) left = right = 0;
+        }
+        return ans;
+    }
+
+    
+    public ListNode mergeKLists(ListNode[] lists) {
+
+    }
+
+
 }
