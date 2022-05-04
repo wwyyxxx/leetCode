fd80f7770c583c702174595deb0ee6db38180de7
Tungbo <1023589809@qq.com>
2022-05-04 23:01:37 +0800

    接雨水

--- java/Solution.java
+++ java/Solution.java
@@ -2740,4 +2740,26 @@ public class Solution {
         }
         return res;
     }
+
+    // 接雨水
+    public int trap(int[] height) {
+        int sum = 0;
+        int[] maxLeft = new int[height.length];
+        int[] maxRight = new int[height.length];
+        for (int i = 1; i < height.length - 1; i++) {
+            maxLeft[i] = Math.max(maxLeft[i-1], height[i- 1]);
+        }
+        for (int i = height.length - 2; i >= 0; i--) {
+            maxRight[i] = Math.max(maxRight[i+1], height[i+ 1]);
+        }
+        for (int i = 1; i < maxRight.length-1; i++) {
+            int min = Math.min(maxLeft[i], maxRight[i]);
+            if(min > height[i]) {
+                sum += (min-height[i]);
+            }
+        }
+        return sum;
+    }
+
+    
 }
