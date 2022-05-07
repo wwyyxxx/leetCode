a19e64c9a1a87b4e1df13ffec7c8d1db538957c0
Tungbo <wangyongxiang@vspn.cn>
2022-05-07 16:15:56 +0800

    每日温度

--- java/Solution.java
+++ java/Solution.java
@@ -2959,4 +2959,20 @@ public class Solution {
             arrayList.remove(arrayList.size() - 1);
         }
     }
+
+    // 每日温度
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] res = new int[temperatures.length];
+        Arrays.fill(res,0);
+        for (int i = 0; i < res.length - 1; i++) {
+            for (int j = i+1; j < res.length; j++) {
+                if(temperatures[j] > temperatures[i]) {
+                    res[i] = j - i; 
+                    break;
+                } else res[i] = 0;
+            }
+        }
+        
+        return res;
+    }
 }
