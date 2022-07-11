cdc6339809ea3952e79a68a684c5480925c03cf0
Tungbo <wangyongxiang@vspn.cn>
2022-07-11 18:43:32 +0800

    165. 比较版本号

--- wyx/TestUtil.java
+++ wyx/TestUtil.java
@@ -9,7 +9,7 @@ import wyx.wyx20220711.MagicDictionary;
 /*
  * @Author: Tungbo
  * @Date: 2021-09-03 15:09:15
- * @LastEditTime: 2022-07-11 11:27:26
+ * @LastEditTime: 2022-07-11 17:12:19
  * @LastEditors: Tungbo
  * @Description: leecode: 
  */
@@ -18,6 +18,7 @@ public class TestUtil {
 
         MagicDictionary m = new MagicDictionary();
         m.buildDict(new String[]{"hello","hallo","leetcode","judge"});
-        System.out.println(m.search("juage"));
+        // System.out.println(m.search("juage"));
+        System.out.println(Integer.valueOf("001"));
     }
 }
