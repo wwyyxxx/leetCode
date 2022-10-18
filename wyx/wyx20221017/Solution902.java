baed6e86cbc1340c266694aa8af7ea6ab276cc41
Tungbo <wangyongxiang@vspn.cn>
2022-10-18 15:40:45 +0800

    902. 最大为 N 的数字组合

---
+++ wyx/wyx20221017/Solution902.java
@@ -0,0 +1,36 @@
+/*
+ * @Author: Tungbo
+ * @Date: 2022-10-18 15:38:53
+ * @LastEditTime: 2022-10-18 15:38:54
+ * @LastEditors: Tungbo
+ * @Description: leecode: 902. 最大为 N 的数字组合
+ * 
+ * 给定一个按 非递减顺序 排列的数字数组 digits 。你可以用任意次数 digits[i] 来写的数字。例如，如果 digits = ['1','3','5']，我们可以写数字，如 '13', '551', 和 '1351315'。
+ * 返回 可以生成的小于或等于给定整数 n 的正整数的个数 
+ * 
+ */
+package wyx.wyx20221017;
+
+public class Solution902 {
+    public int atMostNGivenDigitSet(String[] digits, int n) {
+        String s = Integer.toString(n);
+        int m = digits.length, k = s.length();
+        int[][] dp = new int[k + 1][2];
+        dp[0][1] = 1;
+        for (int i = 1; i <= k; i++) {
+            for (int j = 0; j < m; j++) {
+                if (digits[j].charAt(0) == s.charAt(i - 1)) {
+                    dp[i][1] = dp[i - 1][1];
+                } else if (digits[j].charAt(0) < s.charAt(i - 1)) {
+                    dp[i][0] += dp[i - 1][1];
+                } else {
+                    break;
+                }
+            }
+            if (i > 1) {
+                dp[i][0] += m + dp[i - 1][0] * m;
+            }
+        }
+        return dp[k][0] + dp[k][1];
+    }
+}
