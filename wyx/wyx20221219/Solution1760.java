82f8e305438728e038b1aaf65ee7e3d9dafd867b
Tungbo <1023589809@qq.com>
2022-12-20 18:41:13 +0800

    1760. 袋子里最少数目的球

---
+++ wyx/wyx20221219/Solution1760.java
@@ -0,0 +1,31 @@
+/*
+ * @Author: Tungbo
+ * @Date: 2022-12-20 18:27:35
+ * @LastEditTime: 2022-12-20 18:41:04
+ * @LastEditors: Tungbo
+ * @Description: leecode: 1760. 袋子里最少数目的球
+ */
+package wyx.wyx20221219;
+
+import java.util.Arrays;
+
+public class Solution1760 {
+    public int minimumSize(int[] nums, int maxOperations) {
+        int left = 1, right = Arrays.stream(nums).max().getAsInt();
+        int ans = 0;
+        while (left <= right) {
+            int y = (left + right) / 2;
+            long ops = 0;
+            for (int x : nums) {
+                ops += (x - 1) / y;
+            }
+            if (ops <= maxOperations) {
+                ans = y;
+                right = y - 1;
+            } else {
+                left = y + 1;
+            }
+        }
+        return ans;
+    }
+}
