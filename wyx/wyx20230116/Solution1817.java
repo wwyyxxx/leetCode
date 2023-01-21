6d2163dcd02a66f14267eb73ecbb3b1b9e02906c
Tungbo <1023589809@qq.com>
2023-01-21 19:34:05 +0800

    1824. 最少侧跳次数

--- wyx/wyx20230116/Solution1817.java
+++ wyx/wyx20230116/Solution1817.java
@@ -1,7 +1,7 @@
 /*
  * @Author: Tungbo
  * @Date: 2023-01-20 14:45:57
- * @LastEditTime: 2023-01-20 15:16:09
+ * @LastEditTime: 2023-01-20 15:21:35
  * @LastEditors: Tungbo
  * @Description: leecode: 1817. 查找用户活跃分钟数
  */
@@ -18,9 +18,10 @@ public class Solution1817 {
         Map<Integer,Set<Integer>> ansMap = new HashMap<Integer,Set<Integer>>();
         for (int i = 0; i < logs.length; i++) {
             int[] log = logs[i];
-            Set<Integer> cur = ansMap.getOrDefault(log[0], new HashSet<Integer>());
-            cur.add(log[1]);
-            ansMap.put(log[0],cur);
+            ansMap.computeIfAbsent(log[0], key -> new HashSet<>()).add(log[1]);
+            // Set<Integer> cur = ansMap.getOrDefault(log[0], new HashSet<Integer>());
+            // cur.add(log[1]);
+            // ansMap.put(log[0],cur);
         }
         for (Map.Entry<Integer,Set<Integer>> entry : ansMap.entrySet()) {
             ans[entry.getValue().size() - 1]++;
