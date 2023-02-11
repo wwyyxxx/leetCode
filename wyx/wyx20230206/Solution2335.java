ad72146359e7381c126b5fa66063fdb31ff1464e
Tungbo <1023589809@qq.com>
2023-02-11 15:15:46 +0800

    2335. 装满杯子需要的最短总时长

---
+++ wyx/wyx20230206/Solution2335.java
@@ -0,0 +1,22 @@
+package wyx.wyx20230206;
+
+import java.util.Arrays;
+/*
+ * @Author: Tungbo
+ * @Date: 2023-02-11 12:42:53
+ * @LastEditTime: 2023-02-07 13:04:07
+ * @LastEditors: Tungbo
+ * @Description: leecode: 2335. 装满杯子需要的最短总时长
+ */
+public class Solution2335 {
+    public int fillCups(int[] amount) {
+        int a = amount[0], b = amount[1], c = amount[2];
+        int ans = 0;
+        while(amount[0] > 0 || amount[1] > 0 || amount[2] > 0) {
+            Arrays.sort(amount);
+            amount[2]--;amount[1]--;
+            ans++;
+        }
+        return ans;
+    }
+}
