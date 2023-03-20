eaa45e0019852698be4385c39cf9e654724f9533
Tungbo <1023589809@qq.com>
2023-03-20 11:51:18 +0800

    死锁测试

--- wyx/TestUtil.java
+++ wyx/TestUtil.java
@@ -1,21 +1,15 @@
 /*
  * @Author: Tungbo
  * @Date: 2021-10-10 17:32:11
- * @LastEditTime: 2023-03-12 21:42:49
+ * @LastEditTime: 2023-03-19 00:23:17
  * @LastEditors: Tungbo
  * @Description: leecode: 
  */
 package wyx;
 
 import java.util.*;
-
-import wyx.wyx20221212.Solution1925;
-import wyx.wyx20230109.Solution1807;
-import wyx.wyx20230116.Solution1813;
-import wyx.wyx20230116.Solution1814;
-import wyx.wyx20230116.Solution2299;
-import wyx.wyx20230130.Solution2325;
-import wyx.wyx20230206.Solution1138;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
 
 /*
  * @Author: Tungbo
@@ -25,27 +19,92 @@ import wyx.wyx20230206.Solution1138;
  * @Description: leecode: 
  */
 public class TestUtil {
-    public static void main(String[] args) {
-        System.out.println(new Solution1138().alphabetBoardPath("leet"));
-        Arrays.toString(args);
-    }
+   public static void main(String[] args) {
+      System.out.println("中文测试");
+      // Arrays.toString(args);
+      LockA la = new LockA();
+      new Thread(la).start();
+      LockB lb = new LockB();
+      new Thread(lb).start();
+   }
 
-    public int trap(int[] height) {
-        int ans = 0;
-        int[] maxLeft = new int[height.length];
-        int[] maxRight = new int[height.length];
-        for (int i = 1; i < height.length - 1; i++) {
-            maxLeft[i] = Math.max(maxLeft[i - 1], height[i - 1]);
-        }
-        for (int i = height.length - 2; i >= 0; i--) {
-            maxRight[i] = Math.max(maxRight[i + 1], height[i + 1]);
-        }
-        for (int i = 1; i < height.length - 1; i++) {
-            int min = Math.min(maxLeft[i], maxRight[i]);
-            if (min > height[i]) {
-                ans += (min - height[i]);
+   public int trap(int[] height) {
+      int ans = 0;
+      int[] maxLeft = new int[height.length];
+      int[] maxRight = new int[height.length];
+      for (int i = 1; i < height.length - 1; i++) {
+         maxLeft[i] = Math.max(maxLeft[i - 1], height[i - 1]);
+      }
+      for (int i = height.length - 2; i >= 0; i--) {
+         maxRight[i] = Math.max(maxRight[i + 1], height[i + 1]);
+      }
+      for (int i = 1; i < height.length - 1; i++) {
+         int min = Math.min(maxLeft[i], maxRight[i]);
+         if (min > height[i]) {
+            ans += (min - height[i]);
+         }
+      }
+      return ans;
+   }
+
+   public static String obj1 = "obj1";
+   public static final Semaphore a1 = new Semaphore(1);
+   public static String obj2 = "obj2";
+   public static final Semaphore a2 = new Semaphore(1);
+}
+
+class LockA implements Runnable {
+   public void run() {
+      try {
+         System.out.println(new Date().toString() + " LockA start");
+         while (true) {
+            System.out.println(new Date().toString() + " LockA tryLock LockA");
+            if (TestUtil.a1.tryAcquire(1, TimeUnit.SECONDS)) {
+               System.out.println(new Date().toString() + " LockA lock obj1");
+               Thread.sleep(3000); // 此处等待是给B能锁住机会
+               if (TestUtil.a2.tryAcquire(1, TimeUnit.SECONDS)) {
+                  System.out.println(new Date().toString() + " LockA lock obj2");
+                  Thread.sleep(60 * 1000); // 为测试，占用了就不放
+               }else{
+                  System.out.println(new Date().toString() + "LockA lock obj2 fail");
+               }
+            }else{
+               System.out.println(new Date().toString() + "LockA lock obj1  fail");
             }
-        }
-        return ans;
-    }
+            TestUtil.a1.release(); // 释放
+            TestUtil.a2.release();
+            Thread.sleep(1000); // 马上进行尝试，现实情况下do something是不确定的
+         }
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+   }
+}
+
+class LockB implements Runnable {
+   public void run() {
+      try {
+         System.out.println(new Date().toString() + " LockB start");
+         while (true) {
+            System.out.println(new Date().toString() + " LockA tryLock LockB");
+            if (TestUtil.a2.tryAcquire(1, TimeUnit.SECONDS)) {
+               System.out.println(new Date().toString() + " LockB lock obj2");
+               Thread.sleep(3000); // 此处等待是给A能锁住机会
+               if (TestUtil.a1.tryAcquire(1, TimeUnit.SECONDS)) {
+                  System.out.println(new Date().toString() + " LockB lock obj1");
+                  Thread.sleep(60 * 1000); // 为测试，占用了就不放
+               }else{
+                  System.out.println(new Date().toString() + "LockB lock obj1 fail");
+               }
+            }else{
+               System.out.println(new Date().toString() + "LockB lock obj2 fail");
+            }
+            TestUtil.a1.release(); // 释放
+            TestUtil.a2.release();
+            Thread.sleep(10 * 1000); // 马上进行尝试，现实情况下do something是不确定的
+         }
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+   }
 }
