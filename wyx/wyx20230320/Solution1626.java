8b8014e8ea7519dc978353e7ff9d9b4ad9c8075a
Tungbo <1023589809@qq.com>
2023-03-22 23:10:31 +0800

    补充注释

--- wyx/wyx20230320/Solution1626.java
+++ wyx/wyx20230320/Solution1626.java
@@ -1,7 +1,7 @@
 /*
  * @Author: Tungbo
  * @Date: 2023-03-22 22:24:30
- * @LastEditTime: 2023-03-22 22:46:19
+ * @LastEditTime: 2023-03-22 23:10:19
  * @LastEditors: Tungbo
  * @Description: leecode: 1626. 无矛盾的最佳球队
  */
@@ -28,20 +28,21 @@ public class Solution1626 {
         for (int i = 0; i < ages.length; i++) {
             temp[i] = new int[] { scores[i], ages[i] };
         }
+        //分数升序，年龄升序
         Arrays.sort(temp, new Comparator<int[]>() {
             @Override
             public int compare(int[] p1, int[] p2) {
                 return p2[0] != p1[0] ? p1[0] - p2[0] : p1[1] - p2[1];
             }
         });
-        System.out.println(Arrays.toString(temp));
         int[] dp = new int[ages.length + 1];
         int ans = 0;
         for (int i = 0; i < temp.length; i++) {
             for (int j = 0; j < i; j++) {
+                //寻找年龄比当前小的最佳项
                 if(temp[j][1] <= temp[i][1]) dp[i] = Math.max(dp[i], dp[j]);
             }
-            dp[i] += temp[i][0];
+            dp[i] += temp[i][0]; //累加
             ans = Math.max(ans, dp[i]);
         }
         return ans;
