a0b40adbe6c6405e716f44195ee719579a38bf2b
Tungbo <1023589809@qq.com>
2023-03-30 11:07:19 +0800

    两点之间不包含任何点的最宽垂直区域

---
+++ wyx/wyx20230327/Solution1637.java
@@ -0,0 +1,21 @@
+/*
+ * @Author: Tungbo
+ * @Date: 2023-03-30 11:06:49
+ * @LastEditTime: 2023-03-30 11:07:05
+ * @LastEditors: Tungbo
+ * @Description: leecode: 两点之间不包含任何点的最宽垂直区域
+ */
+package wyx.wyx20230327;
+
+import java.util.Arrays;
+
+public class Solution1637 {
+    public int maxWidthOfVerticalArea(int[][] points) {
+        Arrays.sort(points, (a,b)-> a[0] - b[0]);
+        int diff = 0;
+        for(int i = 0; i < points.length - 1; i++) {
+            diff = Math.max(diff,points[i+1][0] - points[i][0]);
+        }
+        return diff;
+    }
+}
