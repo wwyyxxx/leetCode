6b26d8fd09488fc88b1f3d856d3ae4c517345f0d
Tungbo <1023589809@qq.com>
2023-04-03 11:36:24 +0800

    1053. 交换一次的先前排列

---
+++ wyx/wyx20230403/Solution1053.java
@@ -0,0 +1,27 @@
+/*
+ * @Author: Tungbo
+ * @Date: 2023-04-03 11:36:05
+ * @LastEditTime: 2023-04-03 11:36:09
+ * @LastEditors: Tungbo
+ * @Description: leecode: 1053. 交换一次的先前排列
+ */
+package wyx.wyx20230403;
+
+public class Solution1053 {
+    public int[] prevPermOpt1(int[] arr) {
+        int n = arr.length;
+        for(int i = n - 1; i > 0; i--) {
+            if(arr[i-1] > arr[i]) {
+                for(int j = n-1; j > i - 1;j--) {
+                    if(arr[j] < arr[i - 1] && arr[j] != arr[j-1]) {
+                        int t = arr[i -1];
+                        arr[i -1] = arr[j];
+                        arr[j] = t;
+                        return arr;
+                    }
+                }
+            }
+        }
+        return arr;
+    }
+}
