38a53e17d2c90651e8c11fd795a6d1ac456bb9dc
wangyongxiang <wangyongxiang@zhuanzhuan.com>
2024-02-19 14:31:40 +0800

    590. N 叉树的后序遍历

---
+++ wyx/leetcode2024/wyx20240218/Solution590.java
@@ -0,0 +1,31 @@
+/*
+ * @Author: Tungbo
+ * @Date: 2024-02-19 14:14:14
+ * @LastEditTime: 2024-02-19 14:30:53
+ * @LastEditors: Tungbo
+ * @Description: leecode: 590. N 叉树的后序遍历
+ */
+package wyx.leetcode2024.wyx20240218;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import wyx.bean.Node;
+
+public class Solution590 {
+    public List<Integer> postorder(Node root) {
+        List<Integer> res = new ArrayList<>();
+        dfs(res, root);
+        return res;
+    }
+
+    private void dfs(List<Integer> res, Node root) {
+        if (root == null) {
+            return;
+        }
+        for(Node node : root.children) {
+            dfs(res, node);
+        }
+        res.add(root.val);
+    }
+}
