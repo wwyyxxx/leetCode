501a6a7b71ae97b9893bd37a57266f4ff5cbcd88
wangyongxiang <wangyongxiang@zhuanzhuan.com>
2024-04-01 16:34:56 +0800

    2810. 故障键盘

---
+++ wyx/leetcode2024/wyx20240401/Solution2810.java
@@ -0,0 +1,22 @@
+/*
+ * @Author: Tungbo
+ * @Date: 2024-04-01 16:33:28
+ * @LastEditTime: 2024-04-01 16:34:53
+ * @LastEditors: Tungbo
+ * @Description: leecode: 2810. 故障键盘
+ */
+package wyx.leetcode2024.wyx20240401;
+
+public class Solution2810 {
+    public String finalString(String s) {
+        StringBuilder sb = new StringBuilder();
+        for(char c : s.toCharArray()) {
+            if(c == 'i') {
+                sb.reverse();
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}
