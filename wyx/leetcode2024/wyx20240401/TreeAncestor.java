f57a9fb9609cd8e87fa76b74f7f54715e7c3cf92
Tungbo <1023589809@qq.com>
2024-04-06 19:05:30 +0800

    1483. 树节点的第 K 个祖先

--- wyx/leetcode2024/wyx20240401/TreeAncestor.java
+++ wyx/leetcode2024/wyx20240401/TreeAncestor.java
@@ -9,14 +9,34 @@ package wyx.leetcode2024.wyx20240401;
 
 public class TreeAncestor {
 
-    int[] parent;
+    static final int LOG = 16;
+    int[][] ancestors;
+
     public TreeAncestor(int n, int[] parent) {
-        this.parent = parent;
+        ancestors = new int[n][LOG];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(ancestors[i], -1);
+        }
+        for (int i = 0; i < n; i++) {
+            ancestors[i][0] = parent[i];
+        }
+        for (int j = 1; j < LOG; j++) {
+            for (int i = 0; i < n; i++) {
+                if (ancestors[i][j - 1] != -1) {
+                    ancestors[i][j] = ancestors[ancestors[i][j - 1]][j - 1];
+                }
+            }
+        }            
     }
-    
+
     public int getKthAncestor(int node, int k) {
-        while (k > 0 || node != -1) {
-            node = parent[node];
+        for (int j = 0; j < LOG; j++) {
+            if (((k >> j) & 1) != 0) {
+                node = ancestors[node][j];
+                if (node == -1) {
+                    return -1;
+                }
+            }
         }
         return node;
     }
