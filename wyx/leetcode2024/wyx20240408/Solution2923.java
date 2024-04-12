a6df513319d67b18ef7907489b69fc394c2c9270
wangyongxiang <wangyongxiang@zhuanzhuan.com>
2024-04-12 17:12:42 +0800

    2923. 找到冠军 I

---
+++ wyx/leetcode2024/wyx20240408/Solution2923.java
@@ -0,0 +1,32 @@
+/*
+ * @Author: Tungbo
+ * @Date: 2024-04-12 17:12:08
+ * @LastEditTime: 2024-04-12 17:12:10
+ * @LastEditors: Tungbo
+ * @Description: leecode: 2923. 找到冠军 I
+ */
+package wyx.leetcode2024.wyx20240408;
+
+class Solution2923 {
+    public int findChampion(int[][] grid) {
+        int n = grid.length, m = grid[0].length;
+        next:
+        // for(int i = 0; i < n; i++) {
+        //     for(int j = 0; j < m; j++) {
+        //         if(i != j && grid[i][j] == 0) {
+        //             continue next;
+        //         }
+        //     }
+        //     return i;
+        // }
+        for(int j = 0; j < n; j++) {
+            for(int[] row : grid) {
+                if(row[j] != 0) { continue next;}
+            }
+            return j;
+        }
+
+        return -1;
+    }
+    
+}
