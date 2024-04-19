5278366645ae35ef027aeb2a9dc74124d2681973
wangyongxiang <wangyongxiang@zhuanzhuan.com>
2024-04-19 10:46:40 +0800

    1883. 准时抵达会议现场的最小跳过休息次数

---
+++ wyx/leetcode2024/wyx20240415/Solution1883.java
@@ -0,0 +1,42 @@
+/*
+ * @Author: Tungbo
+ * @Date: 2024-04-19 10:45:05
+ * @LastEditTime: 2024-04-19 10:46:38
+ * @LastEditors: Tungbo
+ * @Description: leecode: 1883. 准时抵达会议现场的最小跳过休息次数
+ */
+package wyx.leetcode2024.wyx20240415;
+
+import java.util.Arrays;
+
+public class Solution1883 {
+    // 可忽略误差
+    static final double EPS = 1e-7;
+    // 极大值
+    static final double INFTY = 1e20;
+
+    public int minSkips(int[] dist, int speed, int hoursBefore) {
+        int n = dist.length;
+        double[][] f = new double[n + 1][n + 1];
+        for (int i = 0; i <= n; ++i) {
+            Arrays.fill(f[i], INFTY);
+        }
+        f[0][0] = 0;
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 0; j <= i; ++j) {
+                if (j != i) {
+                    f[i][j] = Math.min(f[i][j], Math.ceil(f[i - 1][j] + (double) dist[i - 1] / speed - EPS));
+                }
+                if (j != 0) {
+                    f[i][j] = Math.min(f[i][j], f[i - 1][j - 1] + (double) dist[i - 1] / speed);
+                }
+            }
+        }
+        for (int j = 0; j <= n; ++j) {
+            if (f[n][j] < hoursBefore + EPS) {
+                return j;
+            }
+        }
+        return -1;
+    }
+}
