65ad8afe3652a0f203dad590f93a0a93b6afa094
wangyongxiang <wangyongxiang@zhuanzhuan.com>
2024-06-19 19:51:58 +0800

    41. 缺失的第一个正数

---
+++ wyx/leetcode2024/wyx20240617/Solution41.java
@@ -0,0 +1,27 @@
+/*
+ * @Author: Tungbo
+ * @Date: 2024-06-19 19:45:14
+ * @LastEditTime: 2024-06-19 19:51:48
+ * @LastEditors: Tungbo
+ * @Description: leecode: 41. 缺失的第一个正数
+ */
+package wyx.leetcode2024.wyx20240617;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution41 {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        Set<Integer> dic = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            dic.add(nums[i]);
+        }
+        for (int i = 1; i <= n; i++) {
+            if (!dic.contains(i)) {
+                return i;
+            }
+        }
+        return n + 1;
+    }
+}
