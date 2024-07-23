394fc32583ab679ade6a0c63ebad2ae40dc0c863
wangyongxiang <wangyongxiang@zhuanzhuan.com>
2024-07-23 19:34:00 +0800

    35. 搜索插入位置

---
+++ wyx/leetcode2024/wyx20240722/Solution22.java
@@ -0,0 +1,25 @@
+/*
+ * @Author: Tungbo
+ * @Date: 2024-07-23 19:28:26
+ * @LastEditTime: 2024-07-23 19:33:56
+ * @LastEditors: Tungbo
+ * @Description: leecode: 35. 搜索插入位置
+ */
+package wyx.leetcode2024.wyx20240722;
+
+public class Solution22 {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length -1;
+        while (left <= right) {
+            int mid = left + (right - left)/2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+}
