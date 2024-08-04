f98c6fd178ed2bb7c0ca37c3ea6600c3d0d3a895
Tungbo <1023589809@qq.com>
2024-08-04 17:32:17 +0800

    55. 跳跃游戏

---
+++ wyx/leetcode2024/wyx20240729/Solution55.java
@@ -0,0 +1,21 @@
+/*
+ * @Author: Tungbo
+ * @Date: 2024-08-04 17:29:22
+ * @LastEditTime: 2024-08-04 17:31:33
+ * @LastEditors: Tungbo
+ * @Description: leecode: 55. 跳跃游戏
+ */
+package wyx.leetcode2024.wyx20240729;
+
+public class Solution55 {
+    public boolean canJump(int[] nums) {
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (k < i) {
+                return false;
+            }
+            k = Math.max(k, i + nums[i]);
+        }
+        return true;
+    }
+}
