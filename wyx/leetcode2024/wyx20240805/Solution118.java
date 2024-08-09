618f0b3883710ee71f5b2f49fe4c81fd20eb1fb5
wangyongxiang <wangyongxiang@zhuanzhuan.com>
2024-08-09 14:32:21 +0800

    118. 杨辉三角

---
+++ wyx/leetcode2024/wyx20240805/Solution118.java
@@ -0,0 +1,29 @@
+/*
+ * @Author: Tungbo
+ * @Date: 2024-08-09 14:17:22
+ * @LastEditTime: 2024-08-09 14:30:57
+ * @LastEditors: Tungbo
+ * @Description: leecode: 118. 杨辉三角
+ */
+package wyx.leetcode2024.wyx20240805;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution118 {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> path = new ArrayList<>();
+            for (int j = 0; j <= i; ++j) {
+                if (j == 0 || j == i) {
+                    path.add(1);
+                } else {
+                    path.add(res.get(i - 1).get(j - 1) + res.get(i - 1).get(j));
+                }
+            }
+            res.add(path);
+        }
+        return res;
+    }
+}
