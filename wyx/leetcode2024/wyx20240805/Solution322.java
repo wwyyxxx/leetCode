27f0ad8166fb113877c01d8b8c6bc3ce06f4cdfe
wangyongxiang <wangyongxiang@zhuanzhuan.com>
2024-08-13 19:32:55 +0800

    322. 零钱兑换 - 动规

--- wyx/leetcode2024/wyx20240805/Solution322.java
+++ wyx/leetcode2024/wyx20240805/Solution322.java
@@ -1,11 +1,13 @@
 /*
  * @Author: Tungbo
  * @Date: 2024-08-13 16:38:35
- * @LastEditTime: 2024-08-13 17:00:20
+ * @LastEditTime: 2024-08-13 19:31:05
  * @LastEditors: Tungbo
  * @Description: leecode: 322. 零钱兑换
  */
 package wyx.leetcode2024.wyx20240805;
+
+import java.util.Arrays;
 
 public class Solution322 {
     int[] memo;
@@ -40,5 +42,23 @@ public class Solution322 {
         }
         memo[amount - 1] = (min == Integer.MAX_VALUE ? -1 : min);
         return memo[amount - 1];
+    }
+
+    public int coinChange1(int[] coins, int amount) {
+        if (coins.length == 0) {
+            return -1;
+        }
+
+        int[] memo = new int[amount + 1];
+        Arrays.fill(memo, amount + 1);
+        memo[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (i - coins[j] >= 0) {
+                    memo[i] = Math.min(memo[i], memo[i - coins[j]] + 1);
+                }
+            }
+        }
+        return memo[amount] == amount + 1 ? -1 : memo[amount];
     }
 }
