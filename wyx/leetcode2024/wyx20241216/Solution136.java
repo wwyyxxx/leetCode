e05f2b48e76b62fb8fb1e3b53da2d46adff509b2
Tungbo <1023589809@qq.com>
2024-12-16 22:51:01 +0800

    136. 只出现一次的数字

---
+++ wyx/leetcode2024/wyx20241216/Solution136.java
@@ -0,0 +1,18 @@
+/*
+ * @Author: Tungbo
+ * @Date: 2024-12-16 22:40:56
+ * @LastEditTime: 2024-12-16 22:50:56
+ * @LastEditors: Tungbo
+ * @Description: leecode: 136. 只出现一次的数字
+ */
+package wyx.leetcode2024.wyx20241216;
+
+public class Solution136 {
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for(int num : nums) {
+            res ^= num;
+        }
+        return res;
+    }
+}
