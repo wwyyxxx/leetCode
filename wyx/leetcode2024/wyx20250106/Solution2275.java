b008ee61e4029b30bfa651674b091e1b547b5595
Tungbo <1023589809@qq.com>
2025-01-12 00:28:05 +0800

    fix 2275. 按位与结果大于零的最长组合

--- wyx/leetcode2024/wyx20250106/Solution2275.java
+++ wyx/leetcode2024/wyx20250106/Solution2275.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 /*
  * @Author: Tungbo
  * @Date: 2025-01-12 00:18:33
- * @LastEditTime: 2025-01-12 00:18:51
+ * @LastEditTime: 2025-01-12 00:28:00
  * @LastEditors: Tungbo
  * @Description: leecode: 2275. 按位与结果大于零的最长组合
  */
@@ -15,7 +15,7 @@ public class Solution2275 {
         for (int i = 0; i < candidates.length; i++) {
             int candidate = candidates[i];
             for (int j = 0; candidate > 0; j++) {
-                cnt[i] += candidate & 1;
+                cnt[j] += candidate & 1;
                 candidate >>= 1;
             }
         }
