f2522599cedc7de99111576ff5d9d8a1c5a31f6f
Tungbo <1023589809@qq.com>
2025-01-26 23:12:09 +0800

    标准答案

--- wyx/leetcode2024/wyx20250120/Solution40.java
+++ wyx/leetcode2024/wyx20250120/Solution40.java
@@ -15,36 +15,40 @@ import java.util.Set;
 
 public class Solution40 {
 
-    boolean[] vist;
-
-    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        Set<List<Integer>> res = new HashSet<>();
-        vist = new boolean[candidates.length];
-        Arrays.sort(candidates);
-        backtrace(0, res, new ArrayList<Integer>(), candidates, target);
-        return new ArrayList<>(res);
-    }
-
-    private void backtrace(int idx , Set<List<Integer>> res, List<Integer> path, int[] candidates, int target) {
+    void backtrack(List<Integer> state, int target, int[] choices, int start, List<List<Integer>> res) {
+        // 子集和等于 target 时，记录解
         if (target == 0) {
-            res.add(new ArrayList<>(path));
+            res.add(new ArrayList<>(state));
             return;
         }
-
-        if (idx == candidates.length) {
-            return;
-        }
-
-        for (int i = idx; i < candidates.length; i++) {
-            if (vist[i] || (i != 0 && candidates[i] == candidates[i-1])) {
+        // 遍历所有选择
+        // 剪枝二：从 start 开始遍历，避免生成重复子集
+        // 剪枝三：从 start 开始遍历，避免重复选择同一元素
+        for (int i = start; i < choices.length; i++) {
+            // 剪枝一：若子集和超过 target ，则直接结束循环
+            // 这是因为数组已排序，后边元素更大，子集和一定超过 target
+            if (target - choices[i] < 0) {
+                break;
+            }
+            // 剪枝四：如果该元素与左边元素相等，说明该搜索分支重复，直接跳过
+            if (i > start && choices[i] == choices[i - 1]) {
                 continue;
             }
-            int temp = candidates[i];
-            vist[i] = true;
-            path.add(temp);
-            backtrace(idx + 1, res, path, candidates, target - temp);
-            vist[i] = false;
-            path.remove(path.size() -1);
+            // 尝试：做出选择，更新 target, start
+            state.add(choices[i]);
+            // 进行下一轮选择
+            backtrack(state, target - choices[i], choices, i + 1, res);
+            // 回退：撤销选择，恢复到之前的状态
+            state.remove(state.size() - 1);
         }
+    }
+
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<Integer> state = new ArrayList<>(); // 状态（子集）
+        Arrays.sort(candidates); // 对 candidates 进行排序
+        int start = 0; // 遍历起始点
+        List<List<Integer>> res = new ArrayList<>(); // 结果列表（子集列表）
+        backtrack(state, target, candidates, start, res);
+        return res;
     }
 }
