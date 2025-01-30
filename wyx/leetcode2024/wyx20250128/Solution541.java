98f1c4164f3ee4db6c4f3a9293e746130eab631e
Tungbo <1023589809@qq.com>
2025-01-31 01:49:54 +0800

    541. 反转字符串 II

---
+++ wyx/leetcode2024/wyx20250128/Solution541.java
@@ -0,0 +1,36 @@
+package wyx.leetcode2024.wyx20250128;
+
+/*
+ * @Author: Tungbo
+ * @Date: 2025-01-31 01:36:44
+ * @LastEditTime: 2025-01-31 01:49:31
+ * @LastEditors: Tungbo
+ * @Description: leecode: 541. 反转字符串 II
+ */
+public class Solution541 {
+
+    char[] tempDatas;
+
+    public String reverseStr(String s, int k) {
+        tempDatas = s.toCharArray();
+        int n = s.length();
+
+        int start = 0, end = k - 1;
+        while (start < n) {
+            reverse(start, end < n - 1 ? end : n - 1);
+            start += 2 * k;
+            end = start + k - 1;
+        }
+        return new String(tempDatas);
+    }
+
+    private void reverse(int start, int end) {
+        while (start < end) {
+            char temp = tempDatas[start];
+            tempDatas[start] = tempDatas[end];
+            tempDatas[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
