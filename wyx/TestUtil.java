bf8053901c7f5af9132910ef3b544b11c73ce3f7
Tungbo <1023589809@qq.com>
2025-02-03 10:34:25 +0800

    680. 验证回文串 II

--- wyx/TestUtil.java
+++ wyx/TestUtil.java
@@ -29,20 +29,21 @@ import wyx.utils.ExcelUtil;
  */
 public class TestUtil {
    public static void main(String[] args) {
-   //    List<String> list = new ArrayList<>();
-   //    list.add("867578070147345");
-   //    int[] nums = new int[]{1, 2, 3, 3, 4, 5};
-   //    int j = 1;
-   //    for (int i = 1; i < nums.length; i++) {
-   //       if (nums[i] != nums[i - 1]) {
-   //           nums[j++] = nums[i]; // 原地去重
-   //       }
-   //   }
-   //   System.out.println(Arrays.toString(nums));
-   //   System.out.println(isIMEI("867589059240410"));
+      List<String> list = new ArrayList<>();
+      list.add("867578070147345");
+      int[] nums = new int[]{1, 2, 3, 3, 4, 5};
+      int j = 1;
+      for (int i = 1; i < nums.length; i++) {
+         if (nums[i] != nums[i - 1]) {
+             nums[j++] = nums[i]; // 原地去重
+         }
+     }
+     System.out.println(Arrays.toString(nums));
+     System.out.println(isIMEI("867589059240410"));
    
       new Thread(()->{
-         ExcelUtil.readExcel();
+         System.out.println("hello world");
+         // ExcelUtil.readExcel();
          
          // ExcelUtil.compareWebApi();
          // ExcelUtil.compareAndOutputToExcel("wyx", "wyy", "wyx/assets/test.xls");
